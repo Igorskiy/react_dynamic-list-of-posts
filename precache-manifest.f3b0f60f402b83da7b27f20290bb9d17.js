04918f0f76c010d657b29ecf8d3a9fbce0064e92
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-12-20 17:07:30 +0200

    Updates

---
+++ precache-manifest.f3b0f60f402b83da7b27f20290bb9d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1d31eaf75f5d835e89bee86abcad7c",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "ed285919552f7c33925e",
+    "url": "/react_dynamic-list-of-posts/static/css/main.d056862d.chunk.css"
+  },
+  {
+    "revision": "56ca03f88e34a54573be",
+    "url": "/react_dynamic-list-of-posts/static/js/2.15c2d62f.chunk.js"
+  },
+  {
+    "revision": "ed285919552f7c33925e",
+    "url": "/react_dynamic-list-of-posts/static/js/main.ca3479ed.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import PostsList from './PostsList';
-
-ReactDOM.render(<PostsList />, document.getElementById('root'));
